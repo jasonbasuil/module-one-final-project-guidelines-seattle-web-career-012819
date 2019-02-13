e83a10ea812861efa403205881ba4b840f278ad2
jasonbasuil <basuilj@gmail.com>
2019-02-13 11:54:25 -0800

    New menu functionality and status search option

--- bin/CLI.rb
+++ bin/CLI.rb
@@ -18,6 +18,9 @@ class CLI
           browse_pets_by_status
         elsif answer == "3"
           view_favorite_pets
+        elsif answer == "0"
+          puts "Thanks for visiting King County Pet Finder!"
+          exit
         else
           puts "Wrong Choice!"
           return self.main_menu
@@ -28,11 +31,14 @@ class CLI
         puts "Please choose the species you would like to see:"
         puts "Type 1 to see CATS"
         puts "Type 2 to see DOGS"
+        puts "Type 0 to go to the MENU"
         species_choice = gets.chomp
         if species_choice == "1"
             view_species_by_cat()
         elsif species_choice == '2'
             view_species_dog()
+        elsif species_choice == '0'
+            main_menu()
         else
             puts "Please enter a valid option of 1 or 2."
             self.browse_pets_species()
@@ -60,7 +66,8 @@ class CLI
             puts ""
         # binding.pry
         end
-        end
+      end
+      main_menu()
     end
 
     def view_species_dog
@@ -79,9 +86,88 @@ class CLI
               puts ""
           # binding.pry
           end
-          end
+        end
+        main_menu()
     end
 
+    def browse_pets_by_status
+      puts "Please choose an option:"
+      puts "Type 1 to see LOST pets"
+      puts "Type 2 to see FOUND pets"
+      puts "Type 3 to see ADOPTABLE pets"
+      puts "Type 0 to return to the MENU"
+      status_choice = gets.chomp
+      if status_choice == "1"
+          view_status_by_lost()
+      elsif status_choice == '2'
+          view_status_by_found()
+      elsif status_choice == '3'
+          view_status_by_adopted()
+      elsif status_choice == '0'
+          main_menu()
+      else
+          puts "Please enter a valid option of 1 or 2."
+          self.browse_pets_by_status()
+      end
+    end
 
+    def view_status_by_lost
+      response = RestClient.get("https://data.kingcounty.gov/resource/murn-chih.json")
+      pets = JSON.parse(response.body)
+      # binding.pry
+      pets.each do |pet|
+        if !pet["animal_type"].include?("Dead") && pet["record_type"].include?("LOST")
+          puts "Name: " + "#{pet["animal_name"]} "
+          puts "Species: " + "#{pet["animal_type"]} "
+          puts "Breed: " + "#{pet["animal_breed"]} "
+          puts "Age: " + "#{pet["age"]} "
+          puts "Photo: " + "#{pet["image"]} "
+          puts "Information: " + "#{pet["link"]} "
+          puts "Current status: " + "#{pet["record_type"]} "
+          puts ""
+      # binding.pry
+      end
+    end
+    main_menu()
+  end
 
+    def view_status_by_found
+      response = RestClient.get("https://data.kingcounty.gov/resource/murn-chih.json")
+      pets = JSON.parse(response.body)
+      # binding.pry
+      pets.each do |pet|
+        if !pet["animal_type"].include?("Dead") && pet["record_type"].include?("FOUND")
+          puts "Name: " + "#{pet["animal_name"]} "
+          puts "Species: " + "#{pet["animal_type"]} "
+          puts "Breed: " + "#{pet["animal_breed"]} "
+          puts "Age: " + "#{pet["age"]} "
+          puts "Photo: " + "#{pet["image"]} "
+          puts "Information: " + "#{pet["link"]} "
+          puts "Current status: " + "#{pet["record_type"]} "
+          puts ""
+      # binding.pry
+      end
+    end
+    main_menu()
+    end
+
+    def view_status_by_adopted
+      response = RestClient.get("https://data.kingcounty.gov/resource/murn-chih.json")
+      pets = JSON.parse(response.body)
+      # binding.pry
+      pets.each do |pet|
+        if !pet["animal_type"].include?("Dead") && pet["record_type"].include?("ADOPTABLE")
+          puts "Name: " + "#{pet["animal_name"]} "
+          puts "Species: " + "#{pet["animal_type"]} "
+          puts "Breed: " + "#{pet["animal_breed"]} "
+          puts "Age: " + "#{pet["age"]} "
+          puts "Photo: " + "#{pet["image"]} "
+          puts "Information: " + "#{pet["link"]} "
+          puts "Current status: " + "#{pet["record_type"]} "
+          puts ""
+      # binding.pry
+      end
+    end
+    main_menu()
+  end
 end # End CLI
